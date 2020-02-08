b4fccaf8efc62041da4f5ca307de22142bf1b346
Anne <anne.xaymountry.438@my.csun.edu>
2020-02-08 15:03:23 -0800

    finished

--- lab0.c
+++ lab0.c
@@ -1,17 +1,22 @@
+
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-int main(int argc, char** argv[])
+int main(int argc, char** argv)
 {
 
     char *file = malloc(100*sizeof(char));
-    if (argc >= 2 || argv[1]!="-"){
+    if (argc >= 2 || strcmp(argv[1],"-")){
         file = argv[1];
     }
-    
+
     else{
         scanf("%s", file); //string is being stored into file
     }
@@ -22,11 +27,10 @@ int main(int argc, char** argv[])
         printf("FILE NOT FOUND");
         exit(1);
     }
-    char *buf;
-    
+    char buf[0];
+
 
     int i;
-    char bit;
     int numofOnes;
     int dec;
     char ascii;
@@ -35,20 +39,29 @@ int main(int argc, char** argv[])
     printf("Original ASCII    Decimal  Parity\n-------- -------- -------- --------\n");
     int x=1;
     while(x){
-        for (i=0; i<7; i++){
+	i=7;
+	dec=0;
+	numofOnes =0;
+        while (i>=0){
             x = read(fd,buf,1);
-            if(x=0){
-                int j;
-                for(j=i; j<8; j++){
-                    printf('0');
-                }
-                break;
-            }
-            printf(buf);
-            dec += ((int)(bit)-48)*pow(2,i);
 
-            numofOnes += ((int)(bit)-48);
-            
+            if(x==0){
+                int j;
+                for(j=i; j>=0; j--){
+                    printf("0");
+			i--;
+                }
+	       break;
+
+            }
+	if(buf[0] ==' ' || buf[0] =='\0'||buf[0] =='\n' ||buf[0]=='\t'){
+		continue;
+        }
+
+           printf("%c",buf[0]);
+            dec += ((int)(buf[0])-48)*pow(2,i);
+            numofOnes += ((int)(buf[0])-48);
+		i--;
         }
     if(numofOnes%2==0){
         par = "EVEN";
@@ -58,10 +71,10 @@ int main(int argc, char** argv[])
         par = "ODD";
     }
     ascii = (char)(dec);
-    printf("%8c %8d %8s", ascii,dec,par);
-    }
-   
+    printf("%8c %8d %8s\n", ascii,dec,par);
 
+    }
+close(fd);
 return(0);
 
 
