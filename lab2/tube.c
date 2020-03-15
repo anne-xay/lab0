22bd3c5b510f2c180538890457da3c7084200a86
Anne Xaymountry <anne.xaymountry.438@my.csun.edu>
2020-03-14 21:35:45 -0700

    still needs work

--- lab2/tube.c
+++ lab2/tube.c
@@ -3,14 +3,19 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char** argv){
 
-    pid_t child1; //launches two child processes therefor need to check for status of 2 children
+    pid_t child1; //launches two child processes therefore need to check for status of 2 children
     int status1;
     pid_t child2;
     int status2;
 
+    char* newargv1[argc];
+    char* newargv2[argc];
+
+    
     //The array pipefd is used to return two file descriptors referring to the ends of the pipe
     int pipefd[2]; 
 
@@ -19,36 +24,81 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
 
+    int i;
+    for(i = 1; i < argc; i++)
+    {
+        if(strcmp(argv[i], ","))
+        {
+            break;
+        }
+        newargv1[i-1]=argv[i];
+    }
+    newargv1[i] = (char*)0;
+    if(i+1 >= argc)
+    {
+        fprintf(stderr,"Error:Not enough arguements.\n");
+        exit(EXIT_FAILURE);
+    }
+    int j;
+    for(j=1; j+i < argc; j++)
+    {
+        newargv2[j-1]=argv[i+j];
+    }
+    newargv1[i] = (char*)0;
+
     //allocates a pipe
     if (pipe(pipefd) == -1){
         fprintf(stderr,"Error:Pipe Failed\n");
         exit(EXIT_FAILURE);
     }
 
-    //forks two children
+    //forks once
     child1 = fork();
-    child2 = fork();
+    //child2 = fork();
 
-    if(child1<0){
+    if (child1>0){
+        //In parent
+        child2 = fork();
+
+        if(child2<0)
+        {
+            fprintf(stderr,"Error:Fork Failed\n");
+            exit(EXIT_FAILURE); 
+        }
+        else if(child2>0)
+        {
+            //in parent
+            fprintf(stderr, "child 1 %s: $$ = %d\n", newargv1[0], getpid());
+		    fprintf(stderr, "child 2 %s: $$ = %d\n", newargv2[0], getpid());
+
+            close(pipefd[0]);
+
+		    waitpid(child1, &status1, WUNTRACED);
+		    fprintf(stderr, "child 1 %s: $? = %d\n", newargv1[0], status1);
+
+		    waitpid(child2, &status2, WUNTRACED);
+		    fprintf(stderr, "child 2 %s: $? = %d\n", newargv2[2], status2);
+
+		    close(pipefd[1]);
+        }
+        else
+        {
+            //in child2
+            dup2(pipefd[1],1);
+
+            execve(argv[2], newargv2, NULL);
+        }
+    }
+    else if(child1<0){
         fprintf(stderr,"Error:Fork Failed\n");
         exit(EXIT_FAILURE);   
     }
-    else if(child1==0 && child2>0){
+    else{
+        //in child1
         //duplicate file descriptor
         dup2(pipefd[1],1);
-    }
-    else{
-        if (child2<0){
-            fprintf(stderr,"Error:Fork Failed\n");
-            exit(EXIT_FAILURE);
-        }
-        else if (child2==0 && child1>0){
-            dup2(pipefd[1],1);
-        }
-        else{
-            //print
-             fprintf(stderr,"Parent: child1 pid:%d, child2 ppid:%d \n", child1, child2);
-        }
-    }
 
+        execve(argv[1], newargv1, NULL);
+    }
+    return 0;
 }
