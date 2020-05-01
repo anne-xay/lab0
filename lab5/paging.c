9d8c60afbf0c24a33970f4be44be36fc1572fad4
anne_xay <anne.xaymountry.438@my.csun.edu>
2020-04-30 17:14:42 -0700

    Lab5 Submission

--- lab5/paging.c
+++ lab5/paging.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char** argv){
+    int page_num;
+    int offset;
+    //converts string to integer
+    int user_input = atoi(argv[1]);
+
+    page_num = user_input/4096;
+    offset = user_input%4096;
+
+    if(argc <2){
+        fprintf(stderr, "INVALID ARGUEMENTS\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("The address %d contains:\n", user_input);
+    printf("page number = %d\n", page_num);
+    printf("offset = %d\n", offset);
+
+
+}
