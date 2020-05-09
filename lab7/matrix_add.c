c2ddcf78a66287ac040b6a93366666394776f2f3
anne_xay <anne.xaymountry.438@my.csun.edu>
2020-05-08 23:56:16 -0700

    Spaghetti

--- lab7/matrix_add.c
+++ lab7/matrix_add.c
@@ -21,11 +21,6 @@ void matrix_add(int block, int size, int scalar){
 }
 
 /*main: size, blocks
-
-start_time  = get current time;
-scalar        = random number;
-block_size = size / blocks;
-
 for x = 1 .. blocks
    for y = 1 .. blocks
        async read request matrix[x, y]
@@ -41,6 +36,36 @@ emit end_time - start_time
 */
 
 int main(int argc, char** argv) {
+    struct status s;
+    int x;
+    int y;
+
+    
+    /*start_time  = get current time;
+    scalar        = random number;
+    block_size = size / blocks;*/
+
+    //how to get current time
+    s.seconds = time(NULL);
+
+    s.scalar = rand();
+
+    s.block_size = s.size/s.blocks;
+
+    for(x=1; x < s.blocks; x++){
+        for(y=1; i < s.blocks; y++){
+            /*async read request matrix[x, y]
+            block = async read return matrix[x, y]
+            matrix_add(block, block_size, scaler)
+            async write request block
+            async write return block*/
+
+        }
+    }
+
+
+
+
 
 
 }
