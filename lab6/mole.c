7cb88e8a9d280005add49121f0f81a066741f47d
anne_xay <anne.xaymountry.438@my.csun.edu>
2020-05-08 19:51:01 -0700

    Pushing

--- lab6/mole.c
+++ lab6/mole.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    FILE* file;
+    file = fopen("~/lab6.log", "w+") ;
+
+    if (strcmp("mole1",argv[1]) == 0){
+        fprintf(file, "Pop %s\n", argv[1]);
+    }
+    else if(strcmp("mole2",argv[1]) == 0){
+        fprintf(file, "Pop %s\n", argv[1]);
+    }
+
+    fclose(file);
+    return 0;
+
+}
 
